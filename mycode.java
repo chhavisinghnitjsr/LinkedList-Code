d65143e2d444577c5ad2a8c490d8fd8ebd939329
chhavisinghnitjsr <chhavisingh.nitjsr@gmail.com>
2021-06-02 15:45:38 +0530

    MyfirstCode

---
+++ mycode.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class mycode {
+
+    public static void main (String [] args) {
+        System.out.println("Chhavi Singh");
+    }
+
+}
